4719e0184a71f32c6f72b5fea1d131ba49353cd8
Sivaram <sivaram420singleboymail@gmail.com>
2026-03-03 19:22:15 +0530

    Fixed duplicate attribute error

--- Program.cs
+++ Program.cs
@@ -1,17 +1,20 @@
-using DLL1.Controllers;
+using DLL1.Controllers;   // optional (can remove if not needed)
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Middleware
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
