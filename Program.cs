0bc1a43b8a2e8e85eb6fe0990f710cc064769bc8
Sivaram <sivaram420singleboymail@gmail.com>
2026-03-03 12:36:55 +0530

    Fix swagger static files

--- Program.cs
+++ Program.cs
@@ -8,8 +8,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseStaticFiles();   // 🔥 ADD THIS
+
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserPluginAPI v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseRouting();
 app.UseAuthorization();
