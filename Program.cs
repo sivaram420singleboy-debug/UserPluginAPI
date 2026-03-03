22c8546c6527121ea60f866344de983e3bc53dda
Sivaram <sivaram420singleboymail@gmail.com>
2026-03-03 21:12:53 +0530

    Fixed Controllers

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "UserPluginAPI",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
@@ -10,13 +20,13 @@ app.UseSwagger();
 
 app.UseSwaggerUI(c =>
 {
-    c.RoutePrefix = string.Empty;  
+    c.RoutePrefix = string.Empty;
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserPluginAPI v1");
     c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
 });
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
