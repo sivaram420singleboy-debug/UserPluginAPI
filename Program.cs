d3fb5f121f6d59cbe685770f05fd0f65eb22b0b3
Sivaram <sivaram420singleboymail@gmail.com>
2026-03-03 19:54:51 +0530

    Fixed Swagger endpoint

--- Program.cs
+++ Program.cs
@@ -9,9 +9,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseSwagger();
+
 app.UseSwaggerUI(c =>
 {
-    c.RoutePrefix = string.Empty;   // Swagger becomes home page
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserPluginAPI v1");
+    c.RoutePrefix = string.Empty; // Swagger at root
 });
 
 app.UseHttpsRedirection();
