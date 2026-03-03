c7a06b6ded479473707ebc97ce17183a1906561f
Sivaram <sivaram420singleboymail@gmail.com>
2026-03-03 12:29:01 +0530

    Fix swagger render issue

--- Program.cs
+++ Program.cs
@@ -1,38 +1,18 @@
-using System.Reflection;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-var pluginPath = Path.Combine(Directory.GetCurrentDirectory(), "plugins");
-
-if (Directory.Exists(pluginPath))
-{
-    foreach (var dll in Directory.GetFiles(pluginPath, "*.dll"))
-    {
-        var assembly = Assembly.LoadFrom(dll);
-
-        builder.Services
-            .AddControllers()
-            .PartManager
-            .ApplicationParts
-            .Add(new AssemblyPart(assembly));
-    }
-}
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.WebHost.UseUrls("http://0.0.0.0:10000");
 
 var app = builder.Build();
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserPluginAPI v1");
-});
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseRouting();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
