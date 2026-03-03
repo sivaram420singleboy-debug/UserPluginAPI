6f7c2afcf86d45c15ed68a1b8c8930db0ab02473
Sivaram <sivaram420singleboymail@gmail.com>
2026-03-03 20:28:55 +0530

    Fixed Controllers

--- Program.cs
+++ Program.cs
@@ -9,33 +9,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 app.UseSwagger();
-
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserPluginAPI v1");
-    c.RoutePrefix = string.Empty; // Swagger at root
-});
+app.UseSwaggerUI();   // keep swagger at /swagger
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-app.UseDefaultFiles();
-app.UseStaticFiles();
 
 app.MapControllers();
 
-app.MapGet("/Calculator", () =>
-{
-    return Results.Redirect("/api/Calculator/addoperation?a=5&b=3");
-});
-
-app.MapGet("/User", () =>
-{
-    return Results.Redirect("/api/User/getusers");
-});
-
-app.MapGet("/Image", () =>
-{
-    return Results.Redirect("/api/Image/ConvertPNG");
-});
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.Run();
