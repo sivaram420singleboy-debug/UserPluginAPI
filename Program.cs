7155300b9becf92ae44c26f7c3d4180ea27c1086
Sivaram <sivaram420singleboymail@gmail.com>
2026-03-04 11:54:00 +0530

    Fixed Controllers

--- Program.cs
+++ Program.cs
@@ -1,33 +1,20 @@
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "UserPluginAPI",
-        Version = "v1"
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseStaticFiles();   // ⭐ index.html 
+app.UseStaticFiles();   // ⭐ wwwroot files serve ஆக
 
 app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserPluginAPI v1");
-
-    c.RoutePrefix = "index";     // swagger open url
-    c.InjectJavascript("/swagger-custom.js");   // custom script
-});
 app.UseAuthorization();
+
 app.MapControllers();
-app.MapFallbackToFile("index.html");
+
+app.MapFallbackToFile("index.html"); // ⭐ default page
+
 app.Run();
